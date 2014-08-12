6da9bdff88c60cec2f56ec3f39e7a2bc5a20279d
Hitesh Kumar <rohilla.hitesh@gmail.com>
2014-08-12 23:50:33 +0530

    enCypher theme updated

--- test.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>enCypher Technologies Pvt. Ltd.</title>
-</head>
-<body>
-	<h1><?php echo "enCypher Technologies Pvt. Ltd."; ?></h1>
-	<p><?php echo "Welcome to GitHub page of enCypher Technologies Pvt. Ltd." ?></p>
-</body>
-</html>
